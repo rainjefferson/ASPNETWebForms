ffb04b4c424563636fbe2553b7b65ec6bd6956d3
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2023-10-02 23:13:57 -0300

    Cadastro de cliente completo

--- WebFormsASPNET/WebFormsASPNET/CadastroCliente.aspx.cs
+++ WebFormsASPNET/WebFormsASPNET/CadastroCliente.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebFormsASPNET.Dao;
 
 namespace WebFormsASPNET
 {
@@ -16,7 +17,15 @@ namespace WebFormsASPNET
 
         protected void btnCadastrar_Click(object sender, EventArgs e)
         {
+            clientes objClientes = new clientes();
+            objClientes.id = 1;
+            objClientes.cnpj = txtCNPJ.Text;
+            objClientes.razao_social = txtRazaoSocial.Text;
 
+            ClienteDao cliedao = new ClienteDao();
+            cliedao.CadastrarCliente(objClientes);
+
+            gridCliente.DataBind();
         }
     }
 }
---
+++ WebFormsASPNET/WebFormsASPNET/Dao/ClienteDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsASPNET.Dao
+{
+    public class ClienteDao
+    {
+        public void CadastrarCliente(clientes objCliente)
+        {
+            using (webforms_vsEntities ctx = new webforms_vsEntities())
+            {
+                ctx.clientes.Add(objCliente);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
